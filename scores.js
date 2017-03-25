6fc9196be0199809a45fce60c8b2a7ad6a7789cf
Josue Perez <che.perez3@gmail.com>
2017-03-25 15:50:07 -0400

    Changes to index.html, mets.html, new javascript file

---
+++ scores.js
@@ -0,0 +1,24 @@
+function myFunction() {
+    var x = document.getElementById('jalisco');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+        function myFunction2() {
+    var x = document.getElementById('sandiego');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+        function myFunction3() {
+    var x = document.getElementById('losangeles');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
